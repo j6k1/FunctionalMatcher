d0416bd39abb6aefa34653c1ea5c60d901b8580d
jinpu <jinpu@LAPTOP-J1SUAON8>
2017-05-05 21:43:45 +0900

    asciiの範囲の文字（0x00～0x7F）の文字ののみに対応したMatcherを追加

---
+++ src/main/java/FunctionalMatcher/MatcherOfAsciiCharacterClass.java
@@ -0,0 +1,83 @@
+package FunctionalMatcher;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class MatcherOfAsciiCharacterClass<T> implements IMatcherOfCharacterClass<T> {
+	protected IOnMatch<T> callback;
+	protected final boolean[] characterMap;
+
+	protected MatcherOfAsciiCharacterClass(String characters, IOnMatch<T> callback)
+	{
+		characterMap = new boolean[128];
+		Arrays.fill(characterMap, false);
+
+		char[] chars = characters.toCharArray();
+
+		for(char c: chars)
+		{
+			if(c >= 0x80)
+			{
+				throw new InvalidCharacterRangeException(
+							"Character codes outside the range of ascii characters can not be specified.");
+			}
+			characterMap[c] = true;
+		}
+		this.callback = callback;
+	}
+
+	public static <T> MatcherOfAsciiCharacterClass<T> of(String characters, IOnMatch<T> callback)
+	{
+		if(characters == null)
+		{
+			throw new NullReferenceNotAllowedException("The reference to the argument characters is null.");
+		}
+		else if(callback == null)
+		{
+			throw new NullReferenceNotAllowedException("The reference to the argument callback is null.");
+		}
+
+		return new MatcherOfAsciiCharacterClass<T>(characters, callback);
+	}
+
+	public static MatcherOfAsciiCharacterClass<Nothing> of(String characters)
+	{
+		if(characters == null)
+		{
+			throw new NullReferenceNotAllowedException("The reference to the argument characters is null.");
+		}
+
+		return new MatcherOfAsciiCharacterClass<Nothing>(characters, null);
+	}
+
+	@Override
+	public Optional<MatchResult<T>> match(String str, int start, boolean temporary)
+	{
+		if(str == null)
+		{
+			throw new NullReferenceNotAllowedException("A null value was passed as a reference to the content string.");
+		}
+
+		if(start < 0)
+		{
+			throw new InvalidMatchStateException("A negative value was specified for the current position.");
+		}
+		else if(start >= str.length() + 1)
+		{
+			throw new InvalidMatchStateException("The current position is outside the content range.");
+		}
+		else if(start >= str.length() || !characterMap[str.charAt(start)]) return Optional.empty();
+		else if(callback == null || temporary)
+		{
+			return Optional.of(MatchResult.of(new Range(start, start + 1), Optional.empty()));
+		}
+		else
+		{
+			return Optional.of(
+					MatchResult.of(
+							new Range(start, start + 1),
+								Optional.of(callback.onmatch(str,
+											new Range(start, start + 1), Optional.empty()))));
+		}
+	}
+}
