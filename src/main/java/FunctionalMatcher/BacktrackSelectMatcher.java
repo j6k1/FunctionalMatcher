744296e969c52ae80f90022ab8dec80ff293b4e8
jinpu <jinpu@LAPTOP-J1SUAON8>
2018-10-06 16:06:01 +0900

    BacktrackSelectMatcherを追加

---
+++ src/main/java/FunctionalMatcher/BacktrackSelectMatcher.java
@@ -0,0 +1,35 @@
+package FunctionalMatcher;
+
+import java.util.Optional;
+
+public class BacktrackSelectMatcher<T> implements IMatcher<T> {
+	protected final MatcherOfFixedLengthSelect<T> matcher;
+
+	protected BacktrackSelectMatcher(MatcherOfFixedLengthSelect<T> matcher) {
+		this.matcher = matcher;
+	}
+
+	public BacktrackSelectMatcher<T> of(MatcherOfFixedLengthSelect<T> matcher) {
+		return new BacktrackSelectMatcher<T>(matcher);
+	}
+
+	@Override
+	public Optional<MatchResult<T>> match(State state) {
+		for(IFixedLengthMatcher<T> m: matcher)
+		{
+			if(state.start - m.length() >= 0)
+			{
+				Optional<MatchResult<T>> optR = m.match(
+													State.of(state.str,
+															state.start - m.length(),
+															state.temporary));
+
+				if(optR.isPresent()) {
+					return Optional.of(MatchResult.of(new Range(state.start, state.start), Optional.empty()));
+				}
+			}
+		}
+
+		return Optional.empty();
+	}
+}
